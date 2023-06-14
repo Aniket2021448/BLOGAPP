4037b70ac34b8a602fc0463ec4552dedf8c47614
Aniket <108399480+Aniket2021448@users.noreply.github.com>
2023-06-14 16:46:54 +0530

    Step1 completed, ref: Readme.md

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # BLOGAPP
+
+Guide: Dawid paszko
+https://www.youtube.com/watch?v=xKs2IZZya7c
+
+Step1: Made a basic landing page, containing, hardcoded short blogs, and a navbar containing
+the login, register and logo.
+
+Made a commit on 14-06-2023 16:45, after completing the step first.
--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,87 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      test
-    </div>
+      <main>
+        <header>
+          <a href = "" className ="logo">MyBLOGs</a>
+
+          <nav>
+            <a href = "">Login</a>
+            <a href = "">Register</a>
+          </nav>
+
+        </header>
+        {/* A blog post content */}
+        <div className="post">
+          <div>
+            {/* image of the blog, at outer side. */}
+            <img src="https://techcrunch.com/wp-content/uploads/2023/06/GettyImages-1163766417.jpg?w=430&h=230&crop=1" className ="img-fluid rounded-top" alt="blog images"/>
+          </div>
+          <div className = "texts">
+            <h2>
+              {/* title of the blog */}
+              Full hosue battery receiver
+            </h2>
+            <p className = "info">
+              {/* details of tha author */}
+              <a className = "author">Aniket Panchal</a>
+              <time>2021-02-02 16:40 PM</time>
+            </p>
+            <p className = "summary">
+              {/* brief content */}
+            The Philippines’ startup ecosystem is poised for strong growth, thanks to the country’s rising GDP, fast adoption of online services and a new generation of founders.
+            </p>
+          </div>
+        </div>
+
+
+                {/* A blog post content */}
+                <div className="post">
+          <div>
+            {/* image of the blog, at outer side. */}
+            <img src="https://techcrunch.com/wp-content/uploads/2023/06/GettyImages-1163766417.jpg?w=430&h=230&crop=1" className ="img-fluid rounded-top" alt="blog images"/>
+          </div>
+          <div className = "texts">
+            <h2>
+              {/* title of the blog */}
+              Full hosue battery receiver
+            </h2>
+            <p className = "info">
+              {/* details of tha author */}
+              <a className = "author">Aniket Panchal</a>
+              <time>2021-02-02 16:40 PM</time>
+            </p>
+            <p className = "summary">
+              {/* brief content */}
+            The Philippines’ startup ecosystem is poised for strong growth, thanks to the country’s rising GDP, fast adoption of online services and a new generation of founders.
+            </p>
+          </div>
+        </div>
+
+
+                {/* A blog post content */}
+        <div className="post">
+          <div>
+            {/* image of the blog, at outer side. */}
+            <img src="https://techcrunch.com/wp-content/uploads/2023/06/GettyImages-1163766417.jpg?w=430&h=230&crop=1" className ="img-fluid rounded-top" alt="blog images"/>
+          </div>
+          <div className = "texts">
+            <h2>
+              {/* title of the blog */}
+              Full hosue battery receiver
+            </h2>
+            <p className = "info">
+              {/* details of tha author */}
+              <a className = "author">Aniket Panchal</a>
+              <time>2021-02-02 16:40 PM</time>
+            </p>
+            <p className = "summary">
+              {/* brief content */}
+            The Philippines’ startup ecosystem is poised for strong growth, thanks to the country’s rising GDP, fast adoption of online services and a new generation of founders.
+            </p>
+          </div>
+        </div>
+      </main>
   );
 }
 
